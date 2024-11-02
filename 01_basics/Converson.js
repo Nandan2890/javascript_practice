bf128186ac0f69d00252af0eb6c4d5f1cc978c6b
Nandan2890 <nkb2890@gmail.com>
2024-11-02 09:44:54 +0530

    Conversion

---
+++ 01_basics/Converson.js
@@ -0,0 +1,20 @@
+let userName = "Nandan";
+
+let isBooleanUserName = Boolean(userName);
+
+console.log(typeof isBooleanUserName);
+
+// 0=> false ; 1 => true
+// "" => false ; "Nandan" => true
+
+//DATATYPES
+/**
+1. Number
+2. BigInt
+3. Boolean
+4. Object
+5. Undefinied
+6. Null
+7. String
+8. Symbol
+ */
--- 01_basics/Variables.js
+++ 01_basics/Variables.js
@@ -1,3 +1,9 @@
 let userName = "Nandan";
+const userId = "N@ndan";
+var age = 89;
 
 console.log(userName);
+console.log(userId);
+console.log(age);
+
+console.table([userName, userId, age]);
