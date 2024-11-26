58d5c77832100d8c49e0bc6698ae7cd3a5f275ce
Nandan2890 <nkb2890@gmail.com>
2024-11-26 19:50:29 +0530

    modified files 2

--- Javascript/25Nov2024/Script.js
+++ Javascript/25Nov2024/Script.js
@@ -3,7 +3,28 @@
 
 // console.log(x);
 
-let x;
-x ??= 10;
+// let x;
+// x ??= 10;
 
-console.log(x);
+// console.log(x);
+// console.log(typeof x);
+
+/**
+ Java Script functions 
+ */
+
+ // Function to compute the product of p1 and p2
+
+// function myFunction(p1, p2){
+//     return p1 * p2;
+// }
+
+// console.log(myFunction(2, 3));
+
+function myFunction(p, q){
+    return p * q;
+}
+
+// let result = myFunction(2,3);
+
+document.getElementById('demo').innerHTML = `my product of two numbers is ${myFunction(p, q)}.`;
